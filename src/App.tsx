59ef1b41367a925f9f1cccc428ba6b111fb3a8c7
delanoquirino <delanoquirino@gmail.com>
2023-10-01 20:42:35 -0300

    feat: config project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+        <h1 className='text-blue-500 font-bold text-4xl'>
+          Teste
+        </h1>
+    </div>
+  )
+}
+
+export default App
