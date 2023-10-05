94f049243f79c0bb333803e6f5f2a621f88a0428
delanoquirino <delanoquirino@gmail.com>
2023-10-05 01:07:08 -0300

    style: adjust home responsiveness

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,16 +3,14 @@ import { useEffect, useState } from "react";
 
 import { AllRecipeProps } from "../utils/types";
 
-
 import { RecipeByName } from "../components/RecipeByName";
 import { RecipeCategory } from "../components/RecipeCategory";
 import { RecipeRandom } from "../components/RecipeRandom";
 import { Link } from "react-router-dom";
 
-
 export const Home = () => {
   const [allRecipes, setAllRecipes] = useState<AllRecipeProps[]>([]);
-  const [category, setCategory] = useState('Beef')
+  const [category, setCategory] = useState("Beef");
   useEffect(() => {
     axios
       .get(
@@ -30,42 +28,39 @@ export const Home = () => {
   return (
     <div className="dark:bg-[#343541]">
       <RecipeByName />
-      <div className="grid grid-cols-4 gap-5 px-9">
-        <div className="h-full hidden md:block">
+      <div className="flex flex-col md:grid grid-cols-4 gap-2 md:gap-7 px-9 mt-5">
+        <section className="h-full md:block">
           <RecipeCategory onSelectedCategory={setCategory} />
-        </div>
-        <div className="col-span-4 md:col-span-3">
+        </section>
+        <section className="col-span-4 md:col-span-3">
           <RecipeRandom />
-          <div>
-            <h2 className="font-bold text-3xl dark:text-white mt-5">
-            Recipes using: {category}
+          <div className="mt-1 md:mt-10">
+            <h2 className="font-bold text-3xl dark:text-white mt-2 md:mt-5">
+              Recipes using: {category}
             </h2>
-            <div className="mt-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7">
-              {allRecipes ? (
-                allRecipes.map((recipe) => (
-                  <Link to={`/recipe?q=${recipe.idMeal}`}
-                    className=" bg-yellow-200 p-4 md:pb-9 rounded-xl h-full hover:scale-105 transition-all ease-in-out duration-300 cursor-pointer"
-                    key={recipe.idMeal}
-                  >
-                    <img
-                      className="w-full h-[60%] rounded-xl object-cover "
-                      src={recipe.strMealThumb}
-                      alt={recipe.strMeal}
-                    />
-                    <h2 className="lg:text-xl font-bold">{recipe.strMeal}</h2>
-                    <br />
-                      <p className="max-[328px]:text-xs text-lg font-bold ">
-                        Category: {category}
-                      </p>
-                   
-                  </Link>
-                ))
-              ) : (
-                <p>Carregando...</p>
-              )}
-            </div>
+            <section className="mt-5 grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+              {allRecipes.map((recipe) => (
+                <Link
+                  to={`/recipe?q=${recipe.idMeal}`}
+                  className=" bg-yellow-200 p-2 pb-4 rounded-xl h-full hover:scale-105 hover:opacity-80 transition-all ease-in-out duration-300 cursor-pointer"
+                  key={recipe.idMeal}
+                >
+                  <img
+                    className="w-full h-[80%] rounded-xl object-cover "
+                    src={recipe.strMealThumb}
+                    alt={recipe.strMeal}
+                  />
+                  <h2 className="text-xl font-bold truncate">
+                    {recipe.strMeal}
+                  </h2>
+                  <p className="max-[328px]:text-sm text-base font-bold ">
+                    Category: {category}
+                  </p>
+                </Link>
+              ))}
+            </section>
           </div>
-        </div>
+        </section>
       </div>
     </div>
   );
